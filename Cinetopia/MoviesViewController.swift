418729b4a299ca1477a2542b43190dac8c204337
Maria Clara <mariaclara.dg25@gmail.com>
2025-05-22 09:02:30 -0300

    Construindo uma tabela

--- Cinetopia/MoviesViewController.swift
+++ Cinetopia/MoviesViewController.swift
@@ -7,15 +7,51 @@
 
 import UIKit
 
-class MoviesViewController: UIViewController {
+class MoviesViewController: UIViewController, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    var names: [String] = [
+    "Ana", "Giovanna", "Maria", "Lucas"
+    ]
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.backgroundColor = .clear
+        tableView.dataSource = self
+        return tableView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .background
         setupNavigationBar()
-        
+        addSubviews()
+        setupConstraints()
         // Do any additional setup after loading the view.
     }
+    
+    private func addSubviews(){
+        view.addSubview(tableView)
+    }
+    
+    private func setupConstraints(){
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+        
+    }
+    
     private func setupNavigationBar(){
         title = "Filmes Populares"
         navigationController?.navigationBar.prefersLargeTitles = true
