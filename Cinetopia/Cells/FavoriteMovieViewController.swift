6b68384116c561884d8ea5fc019f89459d2857d7
Maria Clara <mariaclara.dg25@gmail.com>
2025-05-28 07:44:14 -0300

    Célula e tela de favoritos

--- Cinetopia/Cells/FavoriteMovieViewController.swift
+++ Cinetopia/Cells/FavoriteMovieViewController.swift
@@ -7,37 +7,86 @@
 
 import UIKit
 
-class FavoriteMovieViewController: UIViewController {
+class FavoriteMovieViewController: UICollectionViewCell {
+    
     
     //MARK: UIComponents
     
-    private let mainView: UIView = {
-        let view = UIView()
-        view.translatesAutoresizingMaskIntoConstraints = false
-        view.backgroundColor = .gray
-        
-        return view
+    private lazy var moviePosterImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFill
+        imageView.layer.cornerRadius = 12
+        imageView.layer.masksToBounds = true
+        return imageView
+    }()
+    
+    private lazy var movieTitleLabel: UILabel = {
+       let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .systemFont(ofSize: 18, weight: .medium)
+        label.textColor = . black
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    private lazy var favoriteButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        let iconImage = UIImage(systemName: "heart.fill")?.withTintColor(.buttonBackground, renderingMode: .alwaysOriginal)
+        button.setImage(iconImage, for: .normal)
+        button.addTarget(self, action: #selector(didTapFvaoriteButton), for: .touchUpInside)
+        return button
     }()
     
     //MARK: View life cycle
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .lightGray
         setupConstraints()
-        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
     
     //MARK: Class methods
     
     private func setupConstraints(){
-        view.addSubview(mainView)
-        mainView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        mainView.topAnchor.constraint(equalTo: view.topAnchor,constant: 100).isActive = true
-        mainView.heightAnchor.constraint(equalToConstant: 200).isActive = true
-        mainView.widthAnchor.constraint(equalToConstant: 150).isActive = true
+        
+        addSubview(moviePosterImageView)
+        moviePosterImageView.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        moviePosterImageView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+        moviePosterImageView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+        moviePosterImageView.heightAnchor.constraint(equalToConstant: 140).isActive = true
+        
+        addSubview(movieTitleLabel)
+        movieTitleLabel.topAnchor.constraint(equalTo: moviePosterImageView.bottomAnchor, constant: 12).isActive = true
+        movieTitleLabel.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+        movieTitleLabel.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+        
+        addSubview(favoriteButton)
+        favoriteButton.topAnchor.constraint(equalTo: movieTitleLabel.bottomAnchor, constant: 7).isActive = true
+        favoriteButton.heightAnchor.constraint(equalToConstant: 25).isActive = true
+        favoriteButton.widthAnchor.constraint(equalToConstant: 25).isActive = true
+        favoriteButton.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        
+    }
+    
+    func setupView(_ movie: Movie){
+        let url = URL(string: movie.image)
+        moviePosterImageView.kf.setImage(with: url)
+        
+        movieTitleLabel.text = movie.title
+    }
+    
+    //MARK: IBAction
+    
+    @objc
+    func didTapFvaoriteButton(_ sender: UIButton){
+        print("did tap favorite button")
     }
 }
 
-#Preview {
-    FavoriteMovieViewController()
-}
